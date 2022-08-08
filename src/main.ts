56e1c11248859bbf31bc75712427e1eacada297d
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-08 17:22:13 +0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // tell nestjs to know class-validators to validate
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(3000);
 }
 bootstrap();
