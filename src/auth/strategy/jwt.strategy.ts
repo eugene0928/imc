d0ec599ddda9d0b6a8b30f650743e787d6a14692
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-09 16:21:25 +0500

    payload type implemented

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { Token } from "../dto";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
         })
     }
 
-    validate(payload: any) {
+    validate(payload: Token) {
         return payload
     }
 }
