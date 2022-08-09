697fd89ad59b2cd0e17f2f110efea0bd911e73c2
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-09 16:16:00 +0500

    all login routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post("login/super_admin")
+    @HttpCode(200)
+    loginSuperAdmin(@Body() dto: AuthDto) {
+        return this.authService.loginSuperAdmin(dto)
+    }
+
+    @Post("login/admin")
+    @HttpCode(200)
+    loginAdmin(@Body() dto: AuthDto) {
+        return this.authService.loginAdmin(dto)
+    }
+
+    @Post("login/teacher") 
+    @HttpCode(200)
+    loginTeacher(@Body() dto: AuthDto) {
+        return this.authService.loginTeacher(dto)
+    }
+
+    @Post("login/student") 
+    @HttpCode(200)
+    loginStudent(@Body() dto: AuthDto) {
+        return this.authService.loginStudent(dto)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,145 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from "argon2";
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {}
 
-    singToken(id: string, name: string ): Promise<string> {
-        return this.jwt.signAsync({ id, name }, {
+    async loginSuperAdmin(dto: AuthDto): Promise<{ status: number, message: string, token: string }> {
+        // get super_admin from db
+        const super_admin = await this.prisma.super_admin.findFirst({ where: { login: dto.login } })
+        // check if exists
+        if(!super_admin) {
+            throw new ForbiddenException({ 
+                status: 403,
+                error: true,
+                message: "Invalid login or password"
+            })
+        }
+        // check the hashed passw with raw passw
+        let psMatched = null
+        try {
+            psMatched = await argon.verify(super_admin.password, dto.password)
+        } catch (error: any) {
+            throw new BadRequestException({
+                status: 400,
+                error: true,
+                message: error.message
+            })
+        }
+        if(!psMatched) {
+            throw new ForbiddenException({
+                status: 403,
+                error: true,
+                message: "Invalid login or password"
+            })
+        }
+
+        return { status: 200, message: "Successfully logged in", token: await this.singToken(super_admin.id, super_admin.name, "super_admin") }
+    }
+
+    async loginAdmin(dto: AuthDto): Promise<{ status: number, message: string, token: string }> {
+        // get admin from db
+        const admin = await this.prisma.admin.findFirst({ where: { login: dto.login } })
+        // check if exists
+        if(!admin) throw new ForbiddenException({ 
+            status: 403,
+            error: true,
+            message: "Invalid login or password"
+        })
+        // check the hashed passw with raw passw
+        let psMatched = null
+        try {
+            psMatched = await argon.verify(admin.password, dto.password)
+        } catch (error: any) {
+            throw new BadRequestException({
+                status: 400,
+                error: true,
+                message: error.message
+            })
+        }
+
+        if(!psMatched) {
+            throw new ForbiddenException({ 
+                status: 403,
+                error: true,
+                message: "Invalid login or password"
+            })
+        }
+
+        return { status: 200, message: "Successfully logged in", token: await this.singToken(admin.id, admin.name, "admin") }
+    }
+
+    async loginTeacher(dto: AuthDto): Promise<{ status: number, message: string, token: string }> {
+        // get teacher from db
+        const teacher = await this.prisma.teacher.findFirst({ where: { login: dto.login } })
+        // check if exists
+        if(!teacher) throw new ForbiddenException({ 
+            status: 403,
+            error: true,
+            message: "Invalid login or password"
+        })
+        // check the hashed passw with raw passw
+        let psMatched = null
+        try {
+            psMatched = await argon.verify(teacher.password, dto.password)
+        } catch (error: any) {
+            throw new BadRequestException({
+                status: 400,
+                error: true,
+                message: error.message
+            })
+        }
+
+        if(!psMatched) {
+            throw new ForbiddenException({
+                status: 403,
+                error: true,
+                message: "Invalid login or password"
+            })
+        }
+
+        return { status: 200, message: "Successfully logged in", token: await this.singToken(teacher.id, teacher.name, "teacher") }
+    }
+
+    async loginStudent(dto: AuthDto): Promise<{ status: number, message: string, token: string }> {
+        // get teacher from db
+        const student = await this.prisma.student.findFirst({ where: { login: dto.login } })
+        // check if exists
+        if(!student) throw new ForbiddenException({ 
+            status: 403,
+            error: true,
+            message: "Invalid login or password"
+        })
+        // check the hashed passw with raw passw
+        let psMatched = null
+        try {
+            psMatched = await argon.verify(student.password, dto.password)
+        } catch (error: any) {
+            throw new BadRequestException({
+                status: 400,
+                error: true,
+                message: error.message
+            })
+        }
+
+        if(!psMatched) {
+            throw new ForbiddenException({
+                status: 403,
+                error: true,
+                message: "Invalid login or password"
+            })
+        }
+
+        return { status: 200, message: "Successfully logged in", token: await this.singToken(student.id, student.name, "student") }
+    }
+
+    singToken(id: string, name: string, status: string ): Promise<string> {
+        return this.jwt.signAsync({ id, name, status }, {
             secret: this.config.get("SECRET_KEY")
         })
     }
