4d6731c4ba364a9aa7d0d4559c1140597778c379
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-10 16:18:00 +0500

    guard is seperated

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,6 +1,24 @@
 import { AuthGuard } from "@nestjs/passport";
 
-export class JwtGuard extends AuthGuard("jwt") {
+export class SuperJwtGuard extends AuthGuard("superjwt") {
+    constructor() {
+        super()
+    }
+}
+
+export class AdminJwtGuard extends AuthGuard("adminjwt") {
+    constructor() {
+        super()
+    }
+}
+
+export class TeacherJwtGuard extends AuthGuard("teacherjwt") {
+    constructor() {
+        super()
+    }
+}
+
+export class StudentJwtGuard extends AuthGuard("studentwt") {
     constructor() {
         super()
     }
