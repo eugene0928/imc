3397112a62c7796b910949f68386cb671f7ada6a
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-10 17:41:18 +0500

    super-admin routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { SuperAdminModule } from './super-admin/super-admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    AuthModule
+    AuthModule,
+    SuperAdminModule
   ],
   controllers: [],
   providers: [],
