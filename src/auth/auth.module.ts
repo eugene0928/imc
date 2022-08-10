f89fc25823868794c74e4d7280559edb2067fa5e
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-10 16:19:24 +0500

    strategies are implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './strategy';
+import { SuperJwtStrategy, AdminJwtStrategy, TeacherJwtStrategy, StudentJwtStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, SuperJwtStrategy, AdminJwtStrategy, TeacherJwtStrategy, StudentJwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
