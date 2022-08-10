264308aedb197ffde91503b0e0e096d944a292df
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-10 16:18:24 +0500

    strategy is divedev into parts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { Token } from "../dto";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
+export class SuperJwtStrategy extends PassportStrategy(Strategy, "superjwt") {
     constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -15,28 +15,76 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
     }
 
     async validate(payload: Token) {
-        if(payload.status == "super_admin") {
-            const super_admin = await this.prisma.super_admin.findUnique({ where: { id: payload.id } })
-            delete super_admin.password
-            return super_admin
-        }
+        // get super from db
+        const super_admin = await this.prisma.super_admin.findFirst({ where: { id: payload.id } })
+        // if they dont exist return null to block the route
+        if(!super_admin) return null
+        // if exists delele hashed passw
+        delete super_admin.password
+        // return super
+        return super_admin
+    }
+}
 
-        if(payload.status == "admin") {
-            const admin =  await this.prisma.admin.findUnique({ where: { id: payload.id } })
-            delete admin.password
-            return admin
-        }
+@Injectable()
+export class AdminJwtStrategy extends PassportStrategy(Strategy, "adminjwt") {
+    constructor(config: ConfigService, private prisma: PrismaService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get("SECRET_KEY"),
+        })
+    }
 
-        if(payload.status == "teacher") {
-            const teacher = await this.prisma.teacher.findUnique({ where: { id: payload.id } })
-            delete teacher.password
-            return teacher
-        }
+    async validate(payload: Token) {
+        // get admin from db
+        const admin = await this.prisma.admin.findFirst({ where: { id: payload.id } })
+        // if not exists return null to block the route
+        if(!admin) return null
+        // if exists delete hashed passw
+        delete admin.password
+        // return admin
+        return admin
+    }
+}
 
-        if(payload.status == "student") {
-            const student = await this.prisma.student.findUnique({ where: { id: payload.id } })
-            delete student.password
-            return student
-        }
+@Injectable()
+export class TeacherJwtStrategy extends PassportStrategy(Strategy, "teacherjwt") {
+    constructor(config: ConfigService, private prisma: PrismaService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get("SECRET_KEY"),
+        })
+    }
+
+    async validate(payload: Token) {
+        // get teacher from db
+        const teacher = await this.prisma.teacher.findFirst({ where: { id: payload.id } })
+        // if not exists return null to block the route
+        if(!teacher) return null
+        // if exists delete hashed passw
+        delete teacher.password
+        // return teacher
+        return teacher
+    }
+}
+
+@Injectable()
+export class StudentJwtStrategy extends PassportStrategy(Strategy, "studentjwt") {
+    constructor(config: ConfigService, private prisma: PrismaService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get("SECRET_KEY"),
+        })
+    }
+
+    async validate(payload: Token) {
+        // get student from db
+        const student = await this.prisma.student.findFirst({ where: { id: payload.id } })
+        // if not exists return null to block the route
+        if(!student) return null
+        // if exists delete hashed passw
+        delete student.password
+        // return student
+        return student
     }
 }
