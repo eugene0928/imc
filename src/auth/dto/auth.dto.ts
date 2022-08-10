4bcae36232adbc615ea62a4d0e52e68158d99c54
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-10 17:39:06 +0500

    admin dto is created

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty } from "class-validator"
+import { IsEmail, IsNotEmpty, IsPhoneNumber, Length, Matches } from "class-validator"
+import { File } from "src/interfaces"
 
 export class AuthDto {
     @IsNotEmpty()
@@ -12,4 +13,33 @@ export class Token {
     id: string
     name: string
     status: string
+}
+
+export class AdminDto {
+    @IsNotEmpty()
+    @Length(3, 255)
+    name: string
+
+    @IsNotEmpty()
+    @Length(3, 255)
+    surname: string
+
+    @IsNotEmpty()
+    login: string
+
+    @IsNotEmpty()
+    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/)
+    password: string 
+
+    @IsEmail() 
+    @IsNotEmpty()
+    email: string 
+
+    @IsNotEmpty() 
+    @IsPhoneNumber("UZ")
+    phone_number: string
+    
+    file: File
+
+    gender?: string
 }
