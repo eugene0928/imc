806381d02bc2cab68a2c32497a601c7f80a93d74
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-11 15:58:09 +0500

    all dtos

---
+++ src/super-admin/dto/admin.dto.ts
@@ -0,0 +1,56 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, Length, Matches } from "class-validator";
+
+export class AdminDto {
+  @IsNotEmpty()
+  @Length(3, 255)
+  name: string;
+
+  @IsNotEmpty()
+  @Length(3, 255)
+  surname: string;
+
+  @IsNotEmpty()
+  login: string;
+
+  @IsNotEmpty()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/)
+  password: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('UZ')
+  phone_number: string;
+
+  file: File;
+
+  gender?: string;
+}
+
+
+export class EditAdminDto {
+  @IsOptional()
+  @Length(3, 255)
+  name?: string
+
+  @IsOptional()
+  @Length(3, 255)
+  surname?: string
+
+  @IsOptional()
+  login?: string
+
+  @IsOptional()
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/)
+  password?: string
+
+  @IsOptional()
+  @IsPhoneNumber('UZ')
+  phone_number?: string
+
+  @IsOptional()
+  @IsEmail()
+  email?: string
+}
