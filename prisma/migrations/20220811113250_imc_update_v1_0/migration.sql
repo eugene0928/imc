5ac2836d9769a016c84fd716a0e28c69154bac1a
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-11 17:17:19 +0500

    bugs fixed

--- prisma/migrations/20220811113250_imc_update_v1_0/migration.sql
+++ prisma/migrations/20220811113250_imc_update_v1_0/migration.sql
@@ -5,9 +5,10 @@ CREATE TABLE "super_admin" (
     "surname" VARCHAR(255) NOT NULL,
     "login" VARCHAR(255) NOT NULL,
     "password" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
 
     CONSTRAINT "super_admin_pkey" PRIMARY KEY ("id")
 );
@@ -19,9 +20,13 @@ CREATE TABLE "admin" (
     "surname" VARCHAR(255) NOT NULL,
     "login" VARCHAR(255) NOT NULL,
     "password" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "gender" VARCHAR(6) DEFAULT 'male',
+    "phone_number" VARCHAR(12) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
     "super_admin_id" UUID NOT NULL,
 
     CONSTRAINT "admin_pkey" PRIMARY KEY ("id")
@@ -35,10 +40,16 @@ CREATE TABLE "teacher" (
     "family_name" VARCHAR(255) NOT NULL,
     "login" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
+    "image" TEXT NOT NULL,
+    "gender" VARCHAR(6) DEFAULT 'male',
+    "phone_number" VARCHAR(12) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "date_of_birth" DATE NOT NULL,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
     "admin_id" UUID NOT NULL,
+    "subject_id" UUID NOT NULL,
 
     CONSTRAINT "teacher_pkey" PRIMARY KEY ("id")
 );
@@ -49,7 +60,7 @@ CREATE TABLE "Group" (
     "name" VARCHAR(1) NOT NULL,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
 
     CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
 );
@@ -70,7 +81,8 @@ CREATE TABLE "faculty" (
     "name" VARCHAR(255) NOT NULL,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
+    "admin_id" UUID NOT NULL,
 
     CONSTRAINT "faculty_pkey" PRIMARY KEY ("id")
 );
@@ -81,10 +93,10 @@ CREATE TABLE "student" (
     "login" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "is_completed" BOOLEAN NOT NULL DEFAULT false,
+    "image" TEXT NOT NULL,
     "name" VARCHAR(255),
     "surname" VARCHAR(255),
-    "family_name" VARCHAR(255),
-    "dateOfBirth" DATE,
+    "date_of_birth" DATE,
     "gender" VARCHAR(10) DEFAULT 'male',
     "phone_number" VARCHAR(13),
     "email" VARCHAR(255),
@@ -97,7 +109,7 @@ CREATE TABLE "student" (
     "date_expired" DATE,
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" DATE NOT NULL,
+    "deleted_at" DATE,
     "faculty_id" UUID NOT NULL,
     "group_id" UUID NOT NULL,
 
@@ -111,6 +123,7 @@ CREATE TABLE "semester" (
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" DATE,
+    "admin_id" UUID NOT NULL,
     "group_id" UUID NOT NULL,
 
     CONSTRAINT "semester_pkey" PRIMARY KEY ("id")
@@ -123,6 +136,7 @@ CREATE TABLE "subject" (
     "created_at" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" DATE,
+    "admin_id" UUID NOT NULL,
 
     CONSTRAINT "subject_pkey" PRIMARY KEY ("id")
 );
@@ -138,18 +152,43 @@ CREATE TABLE "Mark" (
     "deleted_at" DATE,
     "student_id" UUID NOT NULL,
     "semester_id" UUID NOT NULL,
+    "subject_id" UUID NOT NULL,
 
     CONSTRAINT "Mark_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "admin_password_key" ON "admin"("password");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_phone_number_key" ON "admin"("phone_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admin_email_key" ON "admin"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teacher_password_key" ON "teacher"("password");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teacher_phone_number_key" ON "teacher"("phone_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teacher_email_key" ON "teacher"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "student_login_key" ON "student"("login");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Mark_student_id_key" ON "Mark"("student_id");
+CREATE UNIQUE INDEX "student_phone_number_key" ON "student"("phone_number");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Mark_semester_id_key" ON "Mark"("semester_id");
+CREATE UNIQUE INDEX "student_email_key" ON "student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "student_password_series_and_number_key" ON "student"("password_series_and_number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Mark_student_id_key" ON "Mark"("student_id");
 
 -- AddForeignKey
 ALTER TABLE "admin" ADD CONSTRAINT "admin_super_admin_id_fkey" FOREIGN KEY ("super_admin_id") REFERENCES "super_admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -158,10 +197,16 @@ ALTER TABLE "admin" ADD CONSTRAINT "admin_super_admin_id_fkey" FOREIGN KEY ("sup
 ALTER TABLE "teacher" ADD CONSTRAINT "teacher_admin_id_fkey" FOREIGN KEY ("admin_id") REFERENCES "admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "teacher" ADD CONSTRAINT "teacher_subject_id_fkey" FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "GroupTeacher" ADD CONSTRAINT "GroupTeacher_teacher_id_fkey" FOREIGN KEY ("teacher_id") REFERENCES "teacher"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "GroupTeacher" ADD CONSTRAINT "GroupTeacher_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "faculty" ADD CONSTRAINT "faculty_admin_id_fkey" FOREIGN KEY ("admin_id") REFERENCES "admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "student" ADD CONSTRAINT "student_faculty_id_fkey" FOREIGN KEY ("faculty_id") REFERENCES "faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -170,10 +215,19 @@ ALTER TABLE "student" ADD CONSTRAINT "student_faculty_id_fkey" FOREIGN KEY ("fac
 ALTER TABLE "student" ADD CONSTRAINT "student_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "semester" ADD CONSTRAINT "semester_admin_id_fkey" FOREIGN KEY ("admin_id") REFERENCES "admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "semester" ADD CONSTRAINT "semester_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "Group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subject" ADD CONSTRAINT "subject_admin_id_fkey" FOREIGN KEY ("admin_id") REFERENCES "admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Mark" ADD CONSTRAINT "Mark_student_id_fkey" FOREIGN KEY ("student_id") REFERENCES "student"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Mark" ADD CONSTRAINT "Mark_semester_id_fkey" FOREIGN KEY ("semester_id") REFERENCES "semester"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Mark" ADD CONSTRAINT "Mark_subject_id_fkey" FOREIGN KEY ("subject_id") REFERENCES "subject"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
