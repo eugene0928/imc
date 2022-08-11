e6855f74a7ff17a6335a798232811bc85d061332
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-11 15:58:58 +0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
-import { MulterModule } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // give limit for files
-  app.use(bodyParser.json({limit: '5mb'}));
-  app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   // tell nestjs to know class-validators to validate
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // enable cors reqs
   app.enableCors();
   await app.listen(3000);
