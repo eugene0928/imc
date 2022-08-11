abd0f9fdcc35bcbf342f01fa4adc927df7bd1d08
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-11 15:57:39 +0500

    bug fixed

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -14,32 +14,3 @@ export class Token {
     name: string
     status: string
 }
-
-export class AdminDto {
-    @IsNotEmpty()
-    @Length(3, 255)
-    name: string
-
-    @IsNotEmpty()
-    @Length(3, 255)
-    surname: string
-
-    @IsNotEmpty()
-    login: string
-
-    @IsNotEmpty()
-    @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{9,}$/)
-    password: string 
-
-    @IsEmail() 
-    @IsNotEmpty()
-    email: string 
-
-    @IsNotEmpty() 
-    @IsPhoneNumber("UZ")
-    phone_number: string
-    
-    file: File
-
-    gender?: string
-}
