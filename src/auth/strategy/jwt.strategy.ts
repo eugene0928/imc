b5380eafa8dbe9c177d3a0ad6af7be72267eb5d1
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-15 12:41:22 +0500

    minor updates

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class SuperJwtStrategy extends PassportStrategy(Strategy, "superjwt") {
 
     async validate(payload: Token) {
         // get super from db
-        const super_admin = await this.prisma.super_admin.findFirst({ where: { id: payload.id } })
+        const super_admin = await this.prisma.super_admin.findFirst({ where: { id: payload.id, deleted_at: null } })
         // if they dont exist return null to block the route
         if(!super_admin) return null
         // if exists delele hashed passw
@@ -37,7 +37,7 @@ export class AdminJwtStrategy extends PassportStrategy(Strategy, "adminjwt") {
 
     async validate(payload: Token) {
         // get admin from db
-        const admin = await this.prisma.admin.findFirst({ where: { id: payload.id } })
+        const admin = await this.prisma.admin.findFirst({ where: { id: payload.id, deleted_at: null } })
         // if not exists return null to block the route
         if(!admin) return null
         // if exists delete hashed passw
@@ -58,7 +58,7 @@ export class TeacherJwtStrategy extends PassportStrategy(Strategy, "teacherjwt")
 
     async validate(payload: Token) {
         // get teacher from db
-        const teacher = await this.prisma.teacher.findFirst({ where: { id: payload.id } })
+        const teacher = await this.prisma.teacher.findFirst({ where: { id: payload.id, deleted_at: null } })
         // if not exists return null to block the route
         if(!teacher) return null
         // if exists delete hashed passw
@@ -79,7 +79,7 @@ export class StudentJwtStrategy extends PassportStrategy(Strategy, "studentjwt")
 
     async validate(payload: Token) {
         // get student from db
-        const student = await this.prisma.student.findFirst({ where: { id: payload.id } })
+        const student = await this.prisma.student.findFirst({ where: { id: payload.id, deleted_at: null } })
         // if not exists return null to block the route
         if(!student) return null
         // if exists delete hashed passw
