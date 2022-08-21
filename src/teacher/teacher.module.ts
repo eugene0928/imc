283cb38610d7e12c108b917a9fafbbcd021ddef3
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-22 01:06:35 +0500

    teacher route

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TeacherController } from './teacher.controller';
+import { TeacherService } from './teacher.service';
 
 @Module({
-  controllers: [TeacherController]
+  controllers: [TeacherController],
+  providers: [TeacherService]
 })
 export class TeacherModule {}
