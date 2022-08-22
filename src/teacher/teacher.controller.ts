86637befd80aed06c95e358fe1e435af1fe24871
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-22 14:52:04 +0500

    todo methods

--- src/teacher/teacher.controller.ts
+++ src/teacher/teacher.controller.ts
@@ -1,9 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { TeacherJwtGuard } from 'src/auth/guard';
 import { TeacherService } from './teacher.service';
 
 @Controller('teacher')
 export class TeacherController {
     constructor(private teacherService: TeacherService) {}
 
-    
+    @Get("me")
+    @UseGuards(TeacherJwtGuard)
+    getMe() {
+
+    }
+
+    @Get("groups")
+    @UseGuards(TeacherJwtGuard)
+    getGroups() {
+
+    }
+
+    @Get("students/:group/:term")
+    @UseGuards(TeacherJwtGuard)
+    getStudentsByGroup() {
+
+    }
+
+    @Post("mark/:group/mid-term/:subject")
+    @UseGuards(TeacherJwtGuard)
+    markMidTerm() {
+
+    }
+
+    @Patch("mark/final/:term/:id/:subject")
+    @UseGuards(TeacherJwtGuard)
+    markFinalTerm() {
+
+    }
+
+    @Patch("mark/edit/:term/:id/:subject")
+    @UseGuards(TeacherJwtGuard)
+    editMarks() {
+
+    }
 }
