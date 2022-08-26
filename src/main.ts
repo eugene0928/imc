7ec24d4ec65592a036547354a56c81e883829a38
eugene0928 <umid09tuxtayev@gmail.com>
2022-08-26 11:31:30 +0500

    static assets

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // give limit for files
   app.use(bodyParser.json({ limit: '5mb' }));
   app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   // tell nestjs to know class-validators to validate
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // enable cors reqs
+  app.useStaticAssets(join(process.cwd(), "uploads")) 
   app.enableCors();
   await app.listen(3000);
 }
